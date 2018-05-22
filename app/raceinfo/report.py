961742d87b46a73f7b28de3e9bb628871b5d3a67
KarasevAlex <karasev_a_e@mail.ru>
2018-05-23 05:06:59 +0700

    report fix

--- app/raceinfo/report.py
+++ app/raceinfo/report.py
@@ -21,18 +21,24 @@ def Report_show(race_id,isHTML):
         filter(Race.id == race_id).one()
 
     # Jury
-    jury = db.session.query(RaceJury, Jury, JuryType, Nation).\
-        join(Jury, RaceJury.jury_id==Jury.id).\
-        join(JuryType, JuryType.id==RaceJury.jury_function_id). \
-        join(Nation, Jury.nation_id == Nation.id). \
-        filter(RaceJury.race_id == race_id).all()
+
+    jury = db.session.query(Jury.en_lastname.label('en_lastname'),
+                             Jury.en_firstname.label('en_firstname'),
+                             JuryType.type.label('type'),
+                             Nation.name).filter(RaceJury.race_id == race_id,
+                                                 JuryType.id == RaceJury.jury_function_id,
+                                                 RaceJury.jury_id == Jury.id,
+                                                 Jury.nation_id == Nation.id).all()
+
 
     # Course
     course = Course.query.filter(Course.race_id == race_id).first()
-    course_setter = db.session.query(Coursetter, Nation).\
-        join(Nation, Coursetter.nation_id == Nation.id).\
-        filter(Coursetter.id == course.course_coursetter_id).\
-        one()
+
+    course_setter = db.session.query(Coursetter.en_lastname.label('en_lastname'),
+                                        Coursetter.en_firstname.label('en_firstname'),
+                                        Nation.name.label('name'))\
+        .filter(Coursetter.id == course.course_coursetter_id,
+                Coursetter.nation_id == Nation.id).one()
 
     # Competitors
     competitors = db.session.query(Competitor, RaceCompetitor, Nation).\
@@ -42,13 +48,29 @@ def Report_show(race_id,isHTML):
         order_by(RaceCompetitor.bib).\
         all()
 
+    number_of_NOCs = db.session.query(Nation).distinct(Nation.name).\
+        filter(RaceCompetitor.race_id==race_id,
+               RaceCompetitor.competitor_id==Competitor.id,
+               Competitor.nation_code_id==Nation.id).\
+        count()
+    print(number_of_NOCs)
+    forerunners = db.session.query(Forerunner.en_firstname.label('en_firstname'),
+                                   Forerunner.en_lastname.label('en_lastname'),
+                                   CourseForerunner.order.label('order'),
+                                   Nation.name.label('name')).\
+        filter(Forerunner.nation_id == Nation.id,
+               CourseForerunner.forerunner_id == Forerunner.id,
+               CourseForerunner.course_id == course.id).\
+        all()
 
     html_render = render_template('reports/startlist.html',
                                   race=race,
                                   jury=jury,
                                   course=course,
                                   course_setter=course_setter,
-                                  competitors=competitors)
+                                  competitors=competitors,
+                                  forerunners=forerunners,
+                                  number_of_NOCs=number_of_NOCs)
 
     options = {
         'page-size': 'A4',
