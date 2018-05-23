daf70a81b1a3dcad9fb575460765e6b939c94389
KarasevAlex <karasev_a_e@mail.ru>
2018-05-24 04:08:47 +0700

    results report first version

--- app/raceinfo/report.py
+++ app/raceinfo/report.py
@@ -3,6 +3,7 @@ import pdfkit
 from .models import *
 import tempfile
 
+import json
 from . import raceinfo
 
 
@@ -88,9 +89,6 @@ def Report_show(race_id,isHTML):
         response.headers['Content-Type'] = 'application/pdf'
         response.headers['Content-Disposition'] = 'inline; filename=report.pdf'
         return response
-    # string_pdf=io.StringIO(pdf)
-    # return send_file(pdf,mimetype='application/pdf',as_attachment=False)
-
 
 def add_pdf_header(options, race):
     with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as header:
@@ -99,3 +97,77 @@ def add_pdf_header(options, race):
             render_template('reports/header.html', race=race).encode('utf-8')
         )
     return
+
+@raceinfo.route('/race/<int:race_id>/report/result')
+def get_results__(race_id):
+    race_competitors = db.session.query(RaceCompetitor, Competitor, Nation).\
+        join(Competitor). \
+        join(Nation, Competitor.nation_code_id==Nation.id). \
+        filter(RaceCompetitor.race_id==race_id).\
+        all()
+    # Course
+
+    jury = db.session.query(Jury.en_lastname.label('en_lastname'),
+                            Jury.en_firstname.label('en_firstname'),
+                            JuryType.type.label('type'),
+                            Nation.name).filter(RaceJury.race_id == race_id,
+                                                JuryType.id == RaceJury.jury_function_id,
+                                                RaceJury.jury_id == Jury.id,
+                                                Jury.nation_id == Nation.id).all()
+
+    course = Course.query.filter(Course.race_id == race_id).first()
+
+    course_setter = db.session.query(Coursetter.en_lastname.label('en_lastname'),
+                                     Coursetter.en_firstname.label('en_firstname'),
+                                     Nation.name.label('name')) \
+        .filter(Coursetter.id == course.course_coursetter_id,
+                Coursetter.nation_id == Nation.id).one()
+    competitors_approve = db.session.query(ResultApproved.race_competitor_id.label('competitor_id'),
+                                           ResultApproved.time.label('time'),
+                                           ResultApproved.rank.label('rank'),
+                                           ResultApproved.gate.label('gate'),
+                                           ResultApproved.reason.label('reason'),
+                                           RunInfo.number.label('run_number')). \
+        filter(ResultApproved.run_id==RunInfo.id,
+               RunInfo.number.in_([1, 2])).all()
+    qlf_list = []
+    disqlf_list = []
+    for item in race_competitors:
+        if item[0].status_id == 1:
+            qlf_item={
+                'rank': item[0].rank,
+                'bib': item[0].bib,
+                'fiscode': item[1].fiscode,
+                'en_firstname':item[1].en_firstname,
+                'en_lastname': item[1].en_lastname,
+                'birth': item[1].birth,
+                'club': item[0].club,
+                'nation': item[2].name,
+                'total': item[0].time,
+                # 'diff': item[0].diff
+            }
+            for approve in competitors_approve:
+                if item[0].id == approve.competitor_id:
+                    qlf_item['time'+str(approve.run_number)] = approve.time
+                    qlf_item['rank'+str(approve.run_number)] = approve.rank
+            qlf_list.append(qlf_item)
+        else: #Did not start
+            disqlf_list.append({
+                'bib': item[0].bib,
+                'fiscode': item[1].fiscode,
+                'en_firstname': item[1].en_firstname,
+                'en_lastname': item[1].en_lastname,
+                'birth': item[1].birth,
+                'club': item[0].club,
+                'nation': item[2].name,
+                'status': item[0].status_id,
+                'gate': item[0].gate,
+                'reason': item[0].reason
+            })
+    qlf_list=sorted(qlf_list, key= lambda item: item['rank'])
+    return render_template('reports/results.html',
+                           qlf_competitors=qlf_list,
+                           disqlf_competitors=qlf_list,
+                           course=course,
+                           course_setter=course_setter, jury=jury)
+
